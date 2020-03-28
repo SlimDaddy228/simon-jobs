c582cc5cb1fa92404993676189ed2d952ff95180
SlimDaddy228 <60612282+SlimDaddy228@users.noreply.github.com>
2020-03-28 18:22:01 +0200

    Add files via upload

---
+++ diller.lua
@@ -0,0 +1,39 @@
+local cfg = {}
+
+cfg.diller_job = {
+    ["Аддер"] = {"adder","<img src='https://vignette.wikia.nocookie.net/gtawiki/images/9/9e/Adder-GTAV-front.png/revision/latest?cb=20190106193205' height='100' width='200' /><br>Багажник - 20кг</br><br>Мест: 2</br>"},
+    ["Шляген"] = {"schlagen","<img src='https://vignette.wikia.nocookie.net/gtawiki/images/4/42/SchlagenGT-GTAO-front.png/revision/latest?cb=20190107163325' height='100' width='200' /><br>Багажник - 20кг</br><br>Мест: 2</br>"},
+    ["Буллет"] = {"bullet","<img src='https://static.giga.de/wp-content/uploads/2017/07/gta-5-bullit-vapid-rcm600x0.jpg' height='100' width='200' /><br>Багажник - 20кг</br><br>Мест: 2</br>"}
+}
+
+cfg.diller_coords = {
+    ["Диллер"] = {-31.455284118652,-1106.7023925781,26.422357559204}
+}
+
+cfg.cloakroom_types = {
+    ["Автодиллер"] = {
+      _config = {},
+      ["Униформа"] = {
+        [1] = {36,0,2},
+        [2] = {8,1,2},
+        [3] = {166,6,2},
+        [4] = {39,0,2},
+        [5] = {0,0,0},
+        [6] = {25,0,2},
+        [7] = {0,0,0},
+        [8] = {129,0,2},
+        [11] = {66,0,2},
+        ["p2"] = {-1,0}
+      }
+  }
+}
+
+cfg.cloakrooms = {
+    {"Автодиллер", -35.820739746094,-1084.3891601563,26.422328948975},
+}
+
+cfg.repair_coords = {
+    {-37.438816070557,-1088.3146972656,26.42234992981}
+}
+
+return cfg
